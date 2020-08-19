9a7301582dfde19ff3fb76387ccccc3e4b79506a
Sachitha <dilshanjeewantha99@gmail.com>
2020-08-19 22:34:18 +0530

    August 19 victory

--- adz/views.py
+++ adz/views.py
@@ -9,8 +9,8 @@ def advertise(request):
     if request.method == 'GET':
         return render(request,'adz/advertise.html',{'form':AdzForm()})
     else:
-        form = AdzForm(request.POST)
+        form = AdzForm(request.POST,request.FILES or None)
         if form.is_valid():
-            form.save()
-
-        return HttpResponseRedirect('/')
+            newform = form.save(commit=False)
+            newform.save()
+        return render(request,'adz/advertise.html',{'form':AdzForm()})
--- realestate/settings.py
+++ realestate/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
